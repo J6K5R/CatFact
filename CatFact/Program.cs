d4f7bc577bd38c3c88cff061ae4a01e7d516e817
J6K5R <nburdyna@gmail.com>
2026-09-08 22:39:46 +0200

    Basic http get

--- CatFact/Program.cs
+++ CatFact/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+var client = new HttpClient();
+
+var response = await client.GetAsync("https://catfact.ninja/fact");
+
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
