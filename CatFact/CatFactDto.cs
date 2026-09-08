ccbe1de16eee0145b986c66de0f0a9dc410d7604
J6K5R <nburdyna@gmail.com>
2026-09-08 23:34:09 +0200

    Deserialize

---
+++ CatFact/CatFactDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatFact
+{
+    public class CatFactDto
+    {
+        public string fact { get; set; }
+        public int length { get; set; }
+    }
+}
--- CatFact/Program.cs
+++ CatFact/Program.cs
@@ -1,8 +1,19 @@
-﻿
+﻿using System.Text.Json;
+using CatFact;
+
 var client = new HttpClient();
 
 var response = await client.GetAsync("https://catfact.ninja/fact");
 
+Console.WriteLine(response.StatusCode);
+
 var content = await response.Content.ReadAsStringAsync();
 
-Console.WriteLine(content);
+CatFactDto? catfact = JsonSerializer.Deserialize<CatFactDto>(content);
+
+if (catfact == null)
+{
+    return;
+}
+
+Console.WriteLine($"{catfact.length} : {catfact.fact}");
