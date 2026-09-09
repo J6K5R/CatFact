8c84750bcf879f9d900e833f1a6f1227508bb0b8
J6K5R <nburdyna@gmail.com>
2026-09-09 17:57:28 +0200

    catfastservice

---
+++ CatFact/CatFactService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace CatFact
+{
+    public class CatFactService
+    {
+        private HttpClient _httpClient;
+        public CatFactService(HttpClient httpclient)
+        {
+            _httpClient = httpclient;
+        }
+
+        public async Task<CatFactDto?> GetCatFact()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("https://catfact.ninja/fact");
+
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                CatFactDto? catfact = JsonSerializer.Deserialize<CatFactDto>(content);
+
+                if (catfact == null)
+                {
+                    Console.WriteLine("Nie udalo sie pobrac danych");
+                    return null;
+                }
+
+                return catfact;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"{ex.StatusCode} {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- CatFact/Program.cs
+++ CatFact/Program.cs
@@ -4,30 +4,9 @@ using CatFact;
 
 var client = new HttpClient();
 
-try
-{ 
-    var response = await client.GetAsync("https://catfact.ninja/fact");
-
-    response.EnsureSuccessStatusCode();
-
-    var content = await response.Content.ReadAsStringAsync();
-
-    CatFactDto? catfact = JsonSerializer.Deserialize<CatFactDto>(content);
-
-    if (catfact == null)
-    {
-        Console.WriteLine("Nie udalo sie pobrac danych");
-        
-        return;
-    }
-
+/*
     using (StreamWriter outputFile = new StreamWriter("CatFact.txt", true))
     {
         outputFile.WriteLine($"Length: {catfact.length} Fact: {catfact.fact}");
     }
-
-}
-catch (HttpRequestException ex)
-{
-    Console.WriteLine($"{ex.StatusCode} {ex.Message}");
-}
+*/
