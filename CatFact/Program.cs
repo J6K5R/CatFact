e369ead07e268308d605adfc98f9a1d2b41b40cf
J6K5R <nburdyna@gmail.com>
2026-09-09 16:37:10 +0200

    handling exceptions

--- CatFact/Program.cs
+++ CatFact/Program.cs
@@ -1,19 +1,33 @@
 ﻿using System.Text.Json;
+using System.IO;
 using CatFact;
 
 var client = new HttpClient();
 
-var response = await client.GetAsync("https://catfact.ninja/fact");
+try
+{ 
+    var response = await client.GetAsync("https://catfact.ninja/fact");
 
-Console.WriteLine(response.StatusCode);
+    response.EnsureSuccessStatusCode();
 
-var content = await response.Content.ReadAsStringAsync();
+    var content = await response.Content.ReadAsStringAsync();
 
-CatFactDto? catfact = JsonSerializer.Deserialize<CatFactDto>(content);
+    CatFactDto? catfact = JsonSerializer.Deserialize<CatFactDto>(content);
 
-if (catfact == null)
-{
-    return;
+    if (catfact == null)
+    {
+        Console.WriteLine("Nie udalo sie pobrac danych");
+        
+        return;
+    }
+
+    using (StreamWriter outputFile = new StreamWriter("CatFact.txt", true))
+    {
+        outputFile.WriteLine($"Length: {catfact.length} Fact: {catfact.fact}");
+    }
+
 }
-
-Console.WriteLine($"{catfact.length} : {catfact.fact}");
+catch (HttpRequestException ex)
+{
+    Console.WriteLine($"{ex.StatusCode} {ex.Message}");
+}
